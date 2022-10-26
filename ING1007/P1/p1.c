deb5b1c0c4740714e4c653b3fbc7a84e5ca02fa8
Gabriel Rosas <107002901+gabriellsar@users.noreply.github.com>
2022-10-26 16:25:09 -0300

    Update p1.c

--- ING1007/P1/p1.c
+++ ING1007/P1/p1.c
@@ -4,7 +4,7 @@
 
 #define MAX 51
 
-FILE *openFile(cont char *arq, const char *mode);
+FILE *openFile(const char *arq, const char *mode);
 char *monteNome(char *v);
 char *leArquivo(FILE *arq);
 int indexR(char **v, int n);
@@ -31,21 +31,21 @@ int main(void) {
 
   arqB = openFile("tamanho.dat", "wb");
   fwrite(&tam, sizeof(int), 1, arqB);
-  fwrite(strV+index, sizeof(char), tam, arqB);
+  fwrite(*(strV+index), sizeof(char), tam, arqB);
   
   fclose(arqB);
-  libera(novaStr, qtd);
+  libera(strV, qtd);
 
-  char nome[MAX];
+  char nomeV[MAX];
   
   arqB = openFile("tamanho.dat", "rb");
   
   fread(&tam, sizeof(int), 1, arqB);
-  fread(nome, sizeof(char), tam, arqB);
-  nome[tam] = '\0';
+  fread(nomeV, sizeof(char), tam, arqB);
+  nomeV[tam] = '\0';
 
   fclose(arqB);
-  printf("%s",nome);
+  printf("Nome Formatado: %s\n", nomeV);
   
   return 0;
 }
@@ -54,9 +54,9 @@ char *leArquivo(FILE *arq) {
   char nome[MAX], prof[MAX], cid[MAX];
   char *nomeV;
 
-  int np = fscanf(arq, " %[^:]: %[^,], %[^\n]", prof, nome, cid);
-  if (np >= 0 || np < 3) {
-    printf("Erro ao ler os valores");
+  int np = fscanf(arq, " %[^:]: %[^,], %[^\n]\n", prof, nome, cid);
+  if (np >= 0 && np < 3) {
+    printf("Erro ao ler os valores\n");
     exit(1);
   }
 
@@ -66,20 +66,20 @@ char *leArquivo(FILE *arq) {
 
 char *monteNome(char *nome) {
   char *nomeFormatado;
-  int posUltimoNome, b = 0;
+  int posUltimoNome, b = 0, i;
 
-  for (int i = strlen(nome) - 1; i > 0 && nome[i] != ' '; i--) {
-    posUltimoNome = i + 1;
-    for (int j = 0; j > 0; j++) {
-      if (nome[j] == ' ')
-        b++;
-    }
+  for (i = strlen(nome) - 1; i > 0 && nome[i] != ' '; i--);
+  
+  posUltimoNome = i + 1;
+  for (int j = 0; j > 0; j++) {
+    if (nome[j] == ' ')
+      b++;
   }
 
   nomeFormatado =
       (char *)malloc((2 * b + 2 + strlen(nome + posUltimoNome)) * sizeof(char));
   if (nomeFormatado == NULL) {
-    printf("Nao foi possivel alocar memoria");
+    printf("Nao foi possivel alocar memoria\n");
     exit(1);
   }
 
@@ -92,7 +92,7 @@ char *monteNome(char *nome) {
 
   for (int i = 0; i < posUltimoNome - 1; i++) {
     if (nome[i] == ' ') {
-      nomeFormatado[tam++] = nome[i];
+      nomeFormatado[tam++] = nome[i+1];
       nomeFormatado[tam++] = '.';
     }
   }
@@ -107,14 +107,14 @@ int indexR(char **v, int n) {
 
   int index = indexR(v + 1, n - 1);
 
-  if (strlen(*v) > strlen(*(v + index + 1)) {
+  if (strlen(*v) > strlen(*(v + index + 1))) {
     return 0;
   }
   
   return index + 1;
 }
 
-FILE *openFile(cont char *arq, const char *mode) {
+FILE *openFile(const char *arq, const char *mode) {
   FILE *f = fopen(arq, mode);
   if (!f) {
     printf("Erro ao abrir o arquivo");
